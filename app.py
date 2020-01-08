161ae3cf1d54892d776a23a84cfb5cc3d301703b
Jamie Houston <jamie.houston@salesforce.com>
2020-01-07 22:42:56 -0600

    parsing initial data

--- app.py
+++ app.py
@@ -11,12 +11,34 @@ def hello_world():
     MODEL = "MODEL/OPT.CODE"
     EXT_COLOR = "EXTERIOR COLOR"
     INT_COLOR = "INTERIOR COLOR"
+    VIN = "VEHICLE ID NUMBER"
+    PORT = "PORT OF ENTRY"
     url = "https://www.kia.com/us/services/en/windowsticker/load/5XYP5DHC2LG067512"
     r = requests.get(url)
     f = io.BytesIO(r.content)
     p = PyPDF2.PdfFileReader(f)
     pdf_text = p.getPage(0).extractText()
-    return pdf_text[0:pdf_text.index(MODEL)]
+    start = 0
+    end = pdf_text.index(MODEL)
+    model = pdf_text[start:end]
+
+    start = end + len(MODEL) + 1
+    end = pdf_text.index(EXT_COLOR)
+    code = pdf_text[start:end]
+
+    start = end + len(EXT_COLOR) + 1
+    end = pdf_text.index(INT_COLOR)
+    ext_color = pdf_text[start:end]
+
+    start = end + len(INT_COLOR) + 1
+    end = pdf_text.index(VIN)
+    int_color = pdf_text[start:end]
+
+    start = end + len(VIN) + 1
+    end = pdf_text.index(PORT)
+    vin = pdf_text[start:end]
+
+    return f'model: {model} code: {code} ext: {ext_color} int: {int_color} vin: {vin}'
 
 
 if __name__ == '__main__':
---
+++ config.py
@@ -0,0 +1,35 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'I used to know a better password'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+
+
+class TestingConfig(Config):
+    TESTING = True
+
+
+app_config = {
+   'development': DevelopmentConfig,
+   'production': ProductionConfig
+}
