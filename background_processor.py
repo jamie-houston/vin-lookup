eaabcc91e450aa8f7086ab3acd5f37933e5d412d
Jamie Houston <jamie.houston@salesforce.com>
2020-01-10 11:48:40 -0600

    testing 3 minute clock

--- background_processor.py
+++ background_processor.py
@@ -4,10 +4,11 @@ from app.telly_api import repository, vin_generator, vin_service
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=10)
+@sched.scheduled_job('interval', minutes=3)
 def update_cars():
     found = 0
     while (found < 10):
+        print(f"Getting car {found}")
         vin_service.get_next_car()
         found += 1
 
