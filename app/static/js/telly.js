4787e14ca9cc2567827175ea6fe48290ff4010d9
Jamie Houston <jamie.houston@salesforce.com>
2020-01-10 17:21:12 -0600

    links

--- app/static/js/telly.js
+++ app/static/js/telly.js
@@ -15,5 +15,5 @@ function formatDealer(value, row, index){
 }
 
 function formatVin(value, row, index){
-    return "<a href='/car/"+value+"'>"+value+"</a>";
+    return "<a href='/car/"+value+"'>"+value+"</a> - <a href='https://www.kia.com/us/services/en/windowsticker/load/" + value + "' target='_blank'>Sticker</a>";
 }
