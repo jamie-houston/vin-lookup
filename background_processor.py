c68d2762ddcf83775cd6a454673f4633c3857f69
Jamie Houston <jamie.houston@salesforce.com>
2020-01-10 13:58:25 -0600

    config for scraping enabled

--- background_processor.py
+++ background_processor.py
@@ -6,16 +6,18 @@ from app import create_app, db
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=3)
+@sched.scheduled_job('interval', minutes=30)
 def update_cars():
-    config_name = os.getenv('FLASK_CONFIG')
-    app = create_app(config_name)
-    app.app_context().push()
-    found = 0
-    while (found < 10):
-        print(f"Getting car {found}")
-        vin_service.get_next_car()
-        found += 1
+    scraping_enabled = os.getenv("ENABLE_SCRAPING")
+    if (scraping_enabled == 'True'):
+        config_name = os.getenv('FLASK_CONFIG')
+        app = create_app(config_name)
+        app.app_context().push()
+        found = 0
+        while (found < 10):
+            print(f"Getting car {found}")
+            vin_service.get_next_car()
+            found += 1
 
 
 sched.start()
