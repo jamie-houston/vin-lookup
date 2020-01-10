ca75a6ffb24ac8e34049bb404f214112b5fdc522
Jamie Houston <jamie.houston@salesforce.com>
2020-01-10 11:56:56 -0600

    fix for clock

--- background_processor.py
+++ background_processor.py
@@ -1,8 +1,13 @@
+import os
 from apscheduler.schedulers.blocking import BlockingScheduler
 from app.telly_api import repository, vin_generator, vin_service
+from app import create_app, db
 
 sched = BlockingScheduler()
 
+config_name = os.getenv('FLASK_CONFIG')
+app = create_app(config_name)
+app.app_context().push()
 
 @sched.scheduled_job('interval', minutes=3)
 def update_cars():
