cb29a993337d51699deaee9ba3209c9dc005479b
Jamie Houston <jamie.houston@salesforce.com>
2020-01-23 14:20:35 -0600

    scraping more often

--- background_processor.py
+++ background_processor.py
@@ -6,13 +6,15 @@ from app import create_app
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=30)
+@sched.scheduled_job('interval', minutes=10)
 def update_cars():
+    print("Starting scraper")
     scraping_enabled = os.getenv("ENABLE_SCRAPING", False)
-    batch_size = os.getenv("SCRAPER_BATCH_SIZE", 500)
+    batch_size = os.getenv("SCRAPER_BATCH_SIZE", 100)
     if scraping_enabled != 'True':
         print("scraping not enabled")
     else:
+        print(f"Scraping with batch size {batch_size}")
         config_name = os.getenv('FLASK_CONFIG')
         app = create_app(config_name)
         app.app_context().push()
