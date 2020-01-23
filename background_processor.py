730a1142b090bbcac1463623116f2b5a06544770
Jamie Houston <jamie.houston@salesforce.com>
2020-01-23 14:22:07 -0600

    error logging

--- background_processor.py
+++ background_processor.py
@@ -9,16 +9,19 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=10)
 def update_cars():
     print("Starting scraper")
-    scraping_enabled = os.getenv("ENABLE_SCRAPING", False)
-    batch_size = os.getenv("SCRAPER_BATCH_SIZE", 100)
-    if scraping_enabled != 'True':
-        print("scraping not enabled")
-    else:
-        print(f"Scraping with batch size {batch_size}")
-        config_name = os.getenv('FLASK_CONFIG')
-        app = create_app(config_name)
-        app.app_context().push()
-        vin_service.get_next_batch(batch_size)
+    try:
+        scraping_enabled = os.getenv("ENABLE_SCRAPING", False)
+        batch_size = os.getenv("SCRAPER_BATCH_SIZE", 100)
+        if scraping_enabled != 'True':
+            print("scraping not enabled")
+        else:
+            print(f"Scraping with batch size {batch_size}")
+            config_name = os.getenv('FLASK_CONFIG')
+            app = create_app(config_name)
+            app.app_context().push()
+            vin_service.get_next_batch(batch_size)
+    except Exception as e:
+        print(f"Error scraping: {e}")
 
 
 sched.start()
