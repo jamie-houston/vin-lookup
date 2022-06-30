570ff3ef137ebc3cca0f13c87b640fca57bd5f05
tom-g <tomgreene@gmail.com>
2022-06-30 15:38:24 -0500

    Check In

--- app/webapp/views.py
+++ app/webapp/views.py
@@ -26,8 +26,9 @@ def other():
 @webapp.route('/mobile')
 @cache.cached(timeout=500)
 def mobile():
+    scraper_stats = repository.get_scraper_stats()
     cars = repository.get_cars()
-    return render_template("mobile.html", cars=cars)
+    return render_template("mobile.html", cars=cars, scraper_stats=scraper_stats)
 
 
 @webapp.route('/car/<vin>')
@@ -41,14 +42,15 @@ def car_info(vin):
 def dealer_info(dealer_code):
     dealer = repository.get_dealer(dealer_code)
     cars = repository.get_dealer_cars(dealer_code)
-
-    return render_template("dealer.html", dealer=dealer, cars=cars)
+    scraper_stats = repository.get_scraper_stats()
+    return render_template("dealer.html", dealer=dealer, cars=cars, scraper_stats=scraper_stats)
 
 
 @webapp.route('/dealers')
 @cache.cached(timeout=60)
 def dealers():
-    return render_template("dealers.html")
+    scraper_stats = repository.get_scraper_stats()
+    return render_template("dealers.html", scraper_stats=scraper_stats)
 
 
 @webapp.route('/temp')
@@ -57,7 +59,8 @@ def temp_route():
 
 @webapp.route('/cars2')
 def cars2():
-    return render_template("cars2.html")
+    scraper_stats = repository.get_scraper_stats()
+    return render_template("cars2.html", scraper_stats=scraper_stats)
 
 @webapp.route('/hof')
 def hof():
